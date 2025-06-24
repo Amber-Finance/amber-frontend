01400944cb804e78289b34007b179260752b9505
Patricie29 <patricie@marsprotocol.foundation>
2025-06-24 15:43:58 +0700

    tidy: remove unused files

--- apps/app/src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from "@/components/ui/Button";
-export { FlickeringGrid } from "@/components/ui/FlickeringGrid";
-export { default as FormattedValueStatic } from "@/components/ui/FormattedValueStatic";
-export { Particles } from "@/components/ui/Particles";
-export { default as SortIndicator } from "@/components/ui/SortIndicator";
-export { ThemeToggle } from "@/components/ui/ThemeToggle";
-export { default as TokenBalanceStatic } from "@/components/ui/TokenBalanceStatic";
-export { default as Tooltip } from "@/components/ui/Tooltip";
