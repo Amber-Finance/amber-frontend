c2abfa45d26717b443e58cb596127f01da552740
Monkmansteve <sxatzakis@yahoo.gr>
2025-08-15 17:23:46 +0300

    feat: add animated theme toggler component and update ThemeToggle to use it, enhance global styles for view transitions

--- apps/app/src/components/ui/ThemeToggle.tsx
+++ apps/app/src/components/ui/ThemeToggle.tsx
@@ -2,24 +2,12 @@
 
 import * as React from 'react'
 
-import { Moon, Sun } from 'lucide-react'
-
-import { useTheme } from '@/components/providers/ThemeProvider'
-import { Button } from '@/components/ui/Button'
+import { AnimatedThemeToggler } from './animated-theme-toggler'
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
-
   return (
-    <Button
-      variant='outline'
-      size='icon'
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      className='cursor-pointer rounded-full h-8 w-8 !bg-transparent'
-    >
-      <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-primary' />
-      <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-primary' />
-      <span className='sr-only'>Toggle theme</span>
-    </Button>
+    <div className='flex justify-center items-center'>
+      <AnimatedThemeToggler />
+    </div>
   )
 }
