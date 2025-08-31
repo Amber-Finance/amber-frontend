63d3b75f8e81e1cfed3c8d56fbff703fc493ac92
Linkie Link <linkielink.dev@gmail.com>
2025-08-31 19:42:03 +0200

    docs: final og data push

--- apps/docs/src/app/legal/_meta.ts
+++ apps/docs/src/app/legal/_meta.ts
@@ -1,7 +1,6 @@
 export default {
-    index: { title: 'Legal', display: 'hidden' }, // hide landing page
-    terms_of_service: 'Terms of Service',
-    privacy_policy: 'Privacy Policy',
-    cookie_policy: 'Cookie Policy',
-  }
-  
+  index: { title: 'Legal', display: 'hidden' }, // hide landing page
+  terms_of_service: 'Terms of Service',
+  privacy_policy: 'Privacy Policy',
+  cookie_policy: 'Cookie Policy',
+}
